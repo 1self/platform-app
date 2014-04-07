2b6363decc63dd375f4f10adbfe8c7d8f8b830c1
Ed Sykes <ed.sykes@gmail.com>
2014-04-07 08:45:11 +0100

    Added overall average line and icons

--- app/js/build-history-d3.js
+++ app/js/build-history-d3.js
@@ -24,213 +24,246 @@ example_data = function() {
     return data;
 }
 
+d3.json("http://quantifieddev.herokuapp.com/quantifieddev/mydev/HPEYJCCGREGTZOPU")
+    .header("Authorization", "D6B1leGgHLIaACVQZ8ezMt2gUGT1jIY1Tc0FQXfP5HYBvRN50bEeK1ORJu7ckJT82wPmGQeojrKtiXo4HzYdeYANOZWFCfPQyw5CmKrkJyZ6EIMEoSOmvURUyNuqCgbSYhtj11t0VhxduJfOxajMxAJM+eOURleagk0UxVshvJm0cCrnhzqFf9QFQKps4VMdAI9F15BmpchaarOYmEg2whIbLN5uw0xqtQ6BCmpOX3n71ELGTr5rYkeWL1B5V8PcqcC1vmlV7FdOl59vy/Xk64/DrSO6uR/cfKVM/I+ZEDrJ4tcAK2CbfrV8u+wSkRTabAAAdW6OaHFokFl7MLsgHA==")
+    .header("Content-Type", "application/json")
+    .get(function(error, buildHistory) {
+
+        // Transpose the data into layers by cause.
+        var buildsByResult = d3.layout.stack()(["failed", "passed"].map(function(cause) {
+            return buildHistory.map(function(d) {
+                return {
+                    x: parse(d.date),
+                    y: +d[cause]
+                };
+            });
+        }));
+
+        // Compute the x-domain (by date) and y-domain (by top).        
+        x.domain(buildsByResult[0].map(function(d) {
+            return d.x;
+        }));
+        xLinear.domain([0, buildsByResult[0].length]);
+        y.domain([0, d3.max(buildsByResult[buildsByResult.length - 1], function(d) {
+            return d.y0 + d.y;
+        })]);
 
 
-d3.csv("js/crimea.csv", function(crimea) {
-
-    // Transpose the data into layers by cause.
-    var causes = d3.layout.stack()(["failed", "passed"].map(function(cause) {
-        return crimea.map(function(d) {
-            return {
-                x: parse(d.date),
-                y: +d[cause]
-            };
-        });
-    }));
-
-    // Compute the x-domain (by date) and y-domain (by top).
-    x.domain(causes[0].map(function(d) {
-        return d.x;
-    }));
-    xLinear.domain([0, causes[0].length]);
-    y.domain([0, d3.max(causes[causes.length - 1], function(d) {
-        return d.y0 + d.y;
-    })]);
-
-
-    // Add a group for each cause.
-    var cause = svg.selectAll("g.cause")
-        .data(causes)
-        .enter().append("svg:g")
-        .attr("class", "cause")
-        .style("fill", function(d, i) {
-            return z(i);
-        })
-        .style("stroke", function(d, i) {
-            return d3.rgb(z(i)).darker();
-        });
-
-    // Add a rect for each date.
-    var rect = cause.selectAll("rect")
-        .data(Object)
-        .enter().append("svg:rect")
-        .attr("x", function(d) {
-            return x(d.x);
-        })
-        .attr("y", function(d) {
-            return -y(d.y0) - y(d.y);
-        })
-        .attr("height", function(d) {
-            return y(d.y);
-        })
-        .attr("width", x.rangeBand());
-
-    //    Add a label per date
-    var label = svg.selectAll("text.month")
-        .data(x.domain())
-        .enter().append("svg:text")
-        .attr("x", function(d) {
-            return x(d) + x.rangeBand() / 2;
-        })
-        .attr("y", 6)
-        .attr("text-anchor", "middle")
-        .attr("dy", ".71em")
-        .text(function(d, i) {
-            return (i % 7) ? null : formatMonth(d);
-        });
-
-    var filterFormat = format;
-    if (w < 800) {
-        filterFormat = function(d, i) {
-            return (i % 7) ? null : format(d);
-        };
-    }
-
-    var label = svg.selectAll("text.day")
-        .data(x.domain())
-        .enter().append("svg:text")
-        .attr("x", function(d) {
-            return x(d) + x.rangeBand() / 2;
-        })
-        .attr("y", 19)
-        .attr("text-anchor", "middle")
-        .attr("dy", ".71em")
-        .text(filterFormat);
-
-    // Add y-axis rules.
-    var rule = svg.selectAll("g.rule")
-        .data(y.ticks(5))
-        .enter().append("svg:g")
-        .attr("class", "rule")
-        .attr("transform", function(d) {
-            return "translate(0," + -y(d) + ")";
-        });
-
-    rule.append("svg:line")
-        .attr("x2", w - p[1] - p[3])
-        .style("stroke", function(d) {
-            return d ? "#fff" : "#000";
-        })
-        .style("stroke-opacity", function(d) {
-            return d ? .7 : null;
-        });
-
-    rule.append("svg:text")
-        .attr("x", w - p[1] - p[3] + 6)
-        .attr("dy", ".35em")
-        .text(d3.format(",d"));
-
-    // Failed Builds Average:
-    var failedBuildsMovingAverage = d3.svg.line()
-        .x(function(d, i) {
-            //return xLinear(i) * w;
-            return xLinear(i);
-        })
-        .y(function(meanDay, i) {
-            var filteredData = crimea.filter(function(rangeDay, fi) {
-
-
-                var extent = 5;
-                var end = 0;
-                var begin = 5;
-
-                if (day == 0) {
-                    end += 2;
-                    begin += 2;
-                }
-
-                if (day == 6) {
-                    end += 1;
-                    begin += 1;
-                }
-
-                var day = new Date(rangeDay.date).getDay();
-                if (fi > i - 7 && fi <= i) {
-                    if (day != 0 && day != 6) {
-                        return rangeDay;
-                    }
-                }
+        // Add a group for each cause.
+        var cause = svg.selectAll("g.cause")
+            .data(buildsByResult)
+            .enter().append("svg:g")
+            .attr("class", "cause")
+            .style("fill", function(d, i) {
+                return z(i);
             })
-            var curval = d3.mean(filteredData, function(d) {
-                // var day = new Date(d.date).getDay();
-                // if (day > 0 && day < 6) {
-                //     return d.failed;
-                // } else {
-                //     return null;
-                // }
-                return d.failed;
+            .style("stroke", function(d, i) {
+                return d3.rgb(z(i)).darker();
             });
-            return -y(curval); // going up in height so need to go negative
-        })
-        .interpolate("basis");
 
-    svg.append("path")
-        .attr("class", "average")
-        .attr("d", failedBuildsMovingAverage(crimea))
-        .style("fill", "none")
-        .style("stroke", "red")
-        .style("stroke-width", 2);
+        // Add a rect for each date.
+        var rect = cause.selectAll("rect")
+            .data(Object)
+            .enter().append("svg:rect")
+            .attr("x", function(d) {
+                return x(d.x);
+            })
+            .attr("y", function(d) {
+                return -y(d.y0) - y(d.y);
+            })
+            .attr("height", function(d) {
+                return y(d.y);
+            })
+            .attr("width", x.rangeBand());
 
-    // Successful Builds Average:
-    var passedBuildsMovingAverage = d3.svg.line()
-        .x(function(d, i) {
-            //return xLinear(i) * w;
-            return xLinear(i);
-        })
-        .y(function(d, i) {
-            var filteredData = crimea.filter(function(rangeDay, fi) {
+        //    Add a label per date
+        var label = svg.selectAll("text.month")
+            .data(x.domain())
+            .enter().append("svg:text")
+            .attr("x", function(d) {
+                return x(d) + x.rangeBand() / 2;
+            })
+            .attr("y", 6)
+            .attr("text-anchor", "middle")
+            .attr("dy", ".71em")
+            .text(function(d, i) {
+                return (i % 7) ? null : formatMonth(d);
+            });
+
+        var filterFormat = format;
+        if (w < 800) {
+            filterFormat = function(d, i) {
+                return (i % 7) ? null : format(d);
+            };
+        }
+
+        var label = svg.selectAll("text.day")
+            .data(x.domain())
+            .enter().append("svg:text")
+            .attr("x", function(d) {
+                return x(d) + x.rangeBand() / 2;
+            })
+            .attr("y", 19)
+            .attr("text-anchor", "middle")
+            .attr("dy", ".71em")
+            .text(filterFormat);
+
+        // Add y-axis rules.
+        var rule = svg.selectAll("g.rule")
+            .data(y.ticks(5))
+            .enter().append("svg:g")
+            .attr("class", "rule")
+            .attr("transform", function(d) {
+                return "translate(0," + -y(d) + ")";
+            });
+
+        rule.append("svg:line")
+            .attr("x2", w - p[1] - p[3])
+            .style("stroke", function(d) {
+                return d ? "#fff" : "#000";
+            })
+            .style("stroke-opacity", function(d) {
+                return d ? .7 : null;
+            });
+
+        rule.append("svg:text")
+            .attr("x", w - p[1] - p[3] + 6)
+            .attr("dy", ".35em")
+            .text(d3.format(",d"));
+
+        // Failed Builds Average:
+        var failedBuildsMovingAverage = d3.svg.line()
+            .x(function(d, i) {
+                //return xLinear(i) * w;
+                return xLinear(i);
+            })
+            .y(function(meanDay, i) {
+                var filteredData = buildHistory.filter(function(rangeDay, fi) {
 
 
-                var extent = 5;
-                var end = 0;
-                var begin = 5;
+                    var extent = 5;
+                    var end = 0;
+                    var begin = 5;
 
-                if (day == 0) {
-                    end += 2;
-                    begin += 2;
-                }
-
-                if (day == 6) {
-                    end += 1;
-                    begin += 1;
-                }
-
-                var day = new Date(rangeDay.date).getDay();
-                if (fi > i - 7 && fi <= i) {
-                    if (day != 0 && day != 6) {
-                        return rangeDay;
+                    if (day == 0) {
+                        end += 2;
+                        begin += 2;
                     }
-                }
+
+                    if (day == 6) {
+                        end += 1;
+                        begin += 1;
+                    }
+
+                    var day = new Date(rangeDay.date).getDay();
+                    if (fi > i - 7 && fi <= i) {
+                        if (day != 0 && day != 6) {
+                            return rangeDay;
+                        }
+                    }
+                })
+                var curval = d3.mean(filteredData, function(d) {
+                    // var day = new Date(d.date).getDay();
+                    // if (day > 0 && day < 6) {
+                    //     return d.failed;
+                    // } else {
+                    //     return null;
+                    // }
+                    return d.failed;
+                });
+                return -y(curval); // going up in height so need to go negative
+            })
+            .interpolate("basis");
+
+        svg.append("path")
+            .attr("class", "average")
+            .attr("d", failedBuildsMovingAverage(buildHistory))
+            .style("fill", "none")
+            .style("stroke", "red")
+            .style("stroke-width", 2);
+
+        // Successful Builds Average:
+        var passedBuildsMovingAverage = d3.svg.line()
+            .x(function(d, i) {
+                //return xLinear(i) * w;
+                return xLinear(i);
+            })
+            .y(function(d, i) {
+                var filteredData = buildHistory.filter(function(rangeDay, fi) {
+
+
+                    var extent = 5;
+                    var end = 0;
+                    var begin = 5;
+
+                    if (day == 0) {
+                        end += 2;
+                        begin += 2;
+                    }
+
+                    if (day == 6) {
+                        end += 1;
+                        begin += 1;
+                    }
+
+                    var day = new Date(rangeDay.date).getDay();
+                    if (fi > i - 7 && fi <= i) {
+                        if (day != 0 && day != 6) {
+                            return rangeDay;
+                        }
+                    }
+                });
+
+                var curval = d3.mean(filteredData, function(d) {
+                    // var day = new Date(d.date).getDay();
+                    // if (day > 0 && day < 6) {
+                    //     return d.passed;
+                    // } else {
+                    //     return null;
+                    // }
+                    return +d.passed + +d.failed;
+                });
+                return -y(curval); // going up in height so need to go negative
+            })
+            .interpolate("basis");
+
+        svg.append("path")
+            .attr("class", "average")
+            .attr("d", passedBuildsMovingAverage(buildHistory))
+            .style("fill", "none")
+            .style("stroke", "blue")
+            .style("stroke-width", 2);
+
+
+        var weekDays = buildHistory.filter(function(day, fi) {
+
+            var dayOfWeek = new Date(day.date).getDay();
+
+            if (dayOfWeek != 0 && dayOfWeek != 6) {
+                return day;
+            }
+        });
+
+        var overallMean = d3.mean(weekDays, function(d) {
+            return +d.passed + +d.failed;
+        });
+
+
+        // Successful Builds Average:
+        var overallAverage = d3.svg.line()
+            .x(function(d, i) {
+                //return xLinear(i) * w;
+                return xLinear(i);
+            })
+            .y(function(d, i) {
+                return -y(overallMean);
             });
 
-            var curval = d3.mean(filteredData, function(d) {
-                // var day = new Date(d.date).getDay();
-                // if (day > 0 && day < 6) {
-                //     return d.passed;
-                // } else {
-                //     return null;
-                // }
-                return +d.passed + +d.failed;
-            });
-            return -y(curval); // going up in height so need to go negative
-        })
-        .interpolate("basis");
+        svg.append("path")
+            .attr("class", "average")
+            .attr("d", overallAverage(buildHistory))
+            .style("fill", "none")
+            .style("stroke", "orange")
+            .style("stroke-width", 2);
 
-    svg.append("path")
-        .attr("class", "average")
-        .attr("d", passedBuildsMovingAverage(crimea))
-        .style("fill", "none")
-        .style("stroke", "blue")
-        .style("stroke-width", 2);
-
-});
+    });
