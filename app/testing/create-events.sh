17509855c5bb01c8cde5389bba7a36c4d423f3c6
Ed Sykes <ed.sykes@gmail.com>
2014-04-14 17:59:29 +0100

    Added test script and changed the landing page for iphones so that it doesn't have sideways scrolling

---
+++ app/testing/create-events.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 51.5072, "long": 0.1275 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 51.4500, "long": -2.5833 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 52.4831, "long": -1.8936 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 55.9531, "long": -3.1889 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 59.9500, "long": 10.7500 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 48.8567, "long": 2.3508 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 41.3833, "long": 2.1833 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 18.5203, "long": 73.8567 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 18.9750, "long": 72.8258 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 40.6700, "long": -73.9400 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 35.6895, "long": 139.6917 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": -37.8136, "long": 144.9631 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": -23.5500, "long": -46.6333 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": -118.2500, "long": 34.0500 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 9.0667, "long": 7.4833 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": 55.7500, "long": 37.6167 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
+
+curl -X POST -H Content-Type:application/json -H Authorization:3ighaqWrNxTPctNUXZ9GZLBf0DaO3c06DYb7rgFv68Rc3hUmBByTSd4e -H Cache-Control:no-cache -H Postman-Token:e81507c3-4699-3123-66f2-2d6552f98c01 -d '{ "dateTime": "2014-03-31T15:28:36.1788806Z", "location": { "lat": -33.8600, "long": 151.2111 }, "actionTags": [ "Build", "Start" ], "objectTags": [ "Computer", "Software" ], "properties": { "Language": "C#", "Environment": "VisualStudio2012" } }' http://quantifieddev.herokuapp.com/stream/KBCUNDHPAJQOBLVX/event
+
+sleep 2
