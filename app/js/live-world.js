b15c3ffd6884934cc94a99c3b93054468248d99f
devikadeshmukh <ddeshmukh@equalexperts.com>
2014-05-20 10:58:18 +0530

    Refactored live-world.js

--- app/js/live-world.js
+++ app/js/live-world.js
@@ -2,30 +2,34 @@ var liveworld = function() {
 
     var liveDurationMins = 60; // default duration of 1 hour
 
-    $('#last-minute').click(function() {
-        liveDurationMins = 1;
-        loadData();
-    });
+    var registerButtonClickHandlers = function() {
+        $('#last-minute').click(function() {
+            liveDurationMins = 1;
+            loadData();
+        });
 
-    $('#last-hour').click(function() {
-        liveDurationMins = 60;
-        loadData();
-    });
+        $('#last-hour').click(function() {
+            liveDurationMins = 60;
+            loadData();
+        });
 
-    $('#last-day').click(function() {
-        liveDurationMins = 60 * 24;
-        loadData()
-    });
+        $('#last-day').click(function() {
+            liveDurationMins = 60 * 24;
+            loadData()
+        });
 
-    $('#last-week').click(function() {
-        liveDurationMins = 60 * 24 * 7;
-        loadData();
-    });
+        $('#last-week').click(function() {
+            liveDurationMins = 60 * 24 * 7;
+            loadData();
+        });
+    };
 
-    var width = $("#live-world").parent().parent().width()
-    height = width,
-    speed = -1e-3,
-    start = Date.now();
+    registerButtonClickHandlers();
+
+    var width = $("#live-world").parent().parent().width();
+    var height = width;
+    var speed = -1e-3;
+    var start = Date.now();
 
     var sphere = {
         type: "Sphere"
@@ -42,6 +46,13 @@ var liveworld = function() {
         .attr("width", width)
         .attr("height", height);
 
+    var context = canvas.node().getContext("2d");
+
+    var path = d3.geo.path()
+        .projection(projection)
+        .context(context);
+
+    d3.select(self.frameElement).style("height", height + "px");
 
     var loadData = function() {
         d3.json("http://quantifieddev.herokuapp.com/live/devbuild/" + liveDurationMins, function(error, builds) {
@@ -68,7 +79,7 @@ var liveworld = function() {
         },
         60000);
 
-    function CircleSize(compile) {
+    var CircleSize = function(compile) {
         var size = Math.random(1, 0.7) * 0.7;
         return function() {
             if (compile.status == 'buildPassed') {
@@ -90,7 +101,7 @@ var liveworld = function() {
 
     var compiles;
 
-    function createCircles() {
+    var createCircles = function() {
         compiles = compileCoords.map(function(compile) {
             var circleSize = new CircleSize(compile);
 
@@ -126,79 +137,72 @@ var liveworld = function() {
 
             return draw;
         });
+        animateCircles();
     }
 
-    d3.json("topo/world-110m.json", function(error, topo) {
-        var land = topojson.feature(topo, topo.objects.land),
-            grid = graticule();
+    var animateCircles = function() {
+        d3.json("topo/world-110m.json", function(error, topo) {
+            var land = topojson.feature(topo, topo.objects.land),
+                grid = graticule();
 
-        d3.timer(function() {
-            //console.log(new Date().toJSON());
-            var λ = (speed * (Date.now() - start) * 2),
-                φ = -15;
+            d3.timer(function() {
+                //console.log(new Date().toJSON());
+                var λ = (speed * (Date.now() - start) * 2),
+                    φ = -15;
 
-            context.clearRect(0, 0, width, height);
+                context.clearRect(0, 0, width, height);
 
-            context.beginPath();
-            path(sphere);
-            context.lineWidth = 3;
-            context.strokeStyle = "#000";
-            context.stroke();
-            context.fillStyle = "#fff";
-            context.fill();
+                context.beginPath();
+                path(sphere);
+                context.lineWidth = 3;
+                context.strokeStyle = "#000";
+                context.stroke();
+                context.fillStyle = "#fff";
+                context.fill();
 
-            context.save();
-            context.translate(width / 2, 0);
-            context.scale(-1, 1);
-            context.translate(-width / 2, 0);
-            projection.rotate([λ + 180, -φ]);
+                context.save();
+                context.translate(width / 2, 0);
+                context.scale(-1, 1);
+                context.translate(-width / 2, 0);
+                projection.rotate([λ + 180, -φ]);
 
-            context.beginPath();
-            path(land);
-            context.fillStyle = "#dadac4";
-            context.fill();
+                context.beginPath();
+                path(land);
+                context.fillStyle = "#dadac4";
+                context.fill();
 
-            context.beginPath();
-            path(grid);
-            context.lineWidth = .5;
-            context.strokeStyle = "rgba(0,0,119,.2)";
-            context.stroke();
+                context.beginPath();
+                path(grid);
+                context.lineWidth = .5;
+                context.strokeStyle = "rgba(0,0,119,.2)";
+                context.stroke();
 
-            context.restore();
-            projection.rotate([λ, φ]);
+                context.restore();
+                projection.rotate([λ, φ]);
 
-            context.beginPath();
-            path(grid);
-            context.lineWidth = .5;
-            context.strokeStyle = "rgba(0,119,119,.2)";
-            context.stroke();
+                context.beginPath();
+                path(grid);
+                context.lineWidth = .5;
+                context.strokeStyle = "rgba(0,119,119,.2)";
+                context.stroke();
 
-            context.beginPath();
-            path(land);
-            context.fillStyle = "rgba(0,100,0,.5)";
-            context.fill();
-            context.lineWidth = .5;
-            context.strokeStyle = "#060";
-            context.stroke();
+                context.beginPath();
+                path(land);
+                context.fillStyle = "rgba(0,100,0,.5)";
+                context.fill();
+                context.lineWidth = .5;
+                context.strokeStyle = "#060";
+                context.stroke();
 
-            if (compiles != undefined) {
-                compiles.forEach(function(drawCompile) {
-                    drawCompile(context);
-                });
-            }
-
+                if (compiles != undefined) {
+                    compiles.forEach(function(drawCompile) {
+                        drawCompile(context);
+                    });
+                }
+            });
         });
-    });
+    }
 
-    var context = canvas.node().getContext("2d");
-
-    var path = d3.geo.path()
-        .projection(projection)
-        .context(context);
-
-
-
-    d3.select(self.frameElement).style("height", height + "px");
 };
 
 liveworld();
