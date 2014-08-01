1ee4e8c969399b10a9bb3ec5f97e2b1f8c9c0f20
renukaraut <rraut@equalexprts.com>
2014-08-01 17:59:35 +0530

    Changes for download page. made it bigger.

--- js/website.js
+++ js/website.js
@@ -1,5 +1,24 @@
 var signup = function() {
-	window.location.href = (window.location.hostname == "localhost") ?
-		"http://localhost:5000/auth/github" :
-		"http://app.quantifieddev.org/auth/github";
+    window.location.href = (window.location.hostname == "localhost") ?
+        "http://localhost:5000/auth/github" :
+        "http://app.quantifieddev.org/auth/github";
 };
+var showTopButton = function() {
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+        var elementOffset = $('#plug-in-conatiner').offset().top;
+        var distance = (elementOffset - scrollTop);
+        if (distance < 20) {
+            $("#top-button").show();
+        } else {
+            $("#top-button").hide();
+        }
+    });
+
+}
+var goToTop = function(){
+	$('html,body').animate({
+          scrollTop: 0
+        }, 500);
+
+}
