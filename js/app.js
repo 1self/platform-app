c0b96ca45a7bdfad507a12f22ae8926f4f840e4f
renukaraut <rraut@equalexprts.com>
2014-08-04 12:20:28 +0530

    Higlighting code is done.Fixed active tab bug. Refactored links function.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 (function() {
     var app = angular.module('main', []);
 
-    app.controller('angular-application', function() {
+    app.controller('angular-application', function($scope, $location, $anchorScroll) {
+        
         this.tab = 1;
         this.selectTab = function(setTab) {
             this.tab = setTab;
         };
-    });
 
+        $scope.$watch(function() {
+                return document.getElementById("codehighlighting");
+            },
+            function(element) {
+                if (element) {
+                    hljs.highlightBlock(document.getElementById("codehighlighting"));
+                }
+            });
+
+        $scope.scrollTo = function(elementId) {
+            $location.hash(elementId);
+            $anchorScroll();
+        };
+
+    });
 })();
