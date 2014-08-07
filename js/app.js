9af6efe67ce119c62910fd3fa4be4422b96ec4c9
renukaraut <rraut@equalexprts.com>
2014-08-07 17:13:06 +0530

    Added carousel to more info page.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,19 @@
     var app = angular.module('main', []);
 
     app.controller('angular-application', function($scope, $location, $anchorScroll) {
-        
+
         $scope.selectTab = function(setTab) {
             this.tab = setTab;
-             $(window).scrollTop(0);
+            $(window).scrollTop(0);
+            if (setTab == 2) {
+
+                setTimeout(
+                    function() {
+                        $('.carousel').carousel({
+                            interval: 2000
+                        })
+                    }, 5000);
+            }
         };
 
         $scope.$watch(function() {
